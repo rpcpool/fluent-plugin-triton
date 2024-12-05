7a327fcb94afe85a5b1806b1a90f4b2c96ed3d1c
Louis-Vincent <louis-vincent@triton.one>
2024-12-05 11:24:23 -0500

    initial commit

---
+++ lib/fluent/plugin/triton.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "triton/version"
+
+module Fluent
+  module Plugin
+    module Triton
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/triton/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Fluent
+  module Plugin
+    module Triton
+      VERSION = "0.1.0"
+    end
+  end
+end
